d70ab752a11bbb3a02f4451df34188ca44baf743
pds <pds@pds.com>
2025-05-22 09:38:06 +0100

    Create a new keymap for the Elora keyboard

---
+++ keyboards/splitkb/elora/keymaps/my-keymap/config.h
@@ -0,0 +1,30 @@
+/* Copyright 2023 splitkb.com <support@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// Myriad boilerplate
+#define MYRIAD_ENABLE
+
+// Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed
+#define DEBOUNCE 5
+
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+// https://www.reddit.com/r/olkb/comments/onsb15/need_helpqmk_turn_off_ledbacklight_and_underglow/
+#define RGB_DISABLE_TIMEOUT 900000 // milliseconds
+#define RGB_DISABLE_WHEN_USB_SUSPENDED 1
+
